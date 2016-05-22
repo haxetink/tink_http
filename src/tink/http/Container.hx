9e22b131d5eec9454657dafcef7acd229c2c5dbf
Juraj Kirchheim <back2dos@googlemail.com>
2016-05-22 14:03:40 +0200

    Start refactoring container API.

--- src/tink/http/Container.hx
+++ src/tink/http/Container.hx
@@ -10,170 +10,27 @@ import tink.io.IdealSink.BlackHole;
 
 import haxe.io.BytesOutput;
 import tink.io.*;
-using StringTools;
 
+using StringTools;
 using tink.CoreApi;
 
 interface Container {
-  function run(application:Application):Void;
+  function run(handler:Handler):Future<ContainerResult>;
 }
 
-#if nodejs
-class NodeContainer implements Container {
-  
-  var port:Int;
-  
-  public function new(port) {
-    this.port = port;
-  }
-  
-  static public function toNodeHandler(handler:IncomingRequest->Future<OutgoingResponse>)
-    return 
-      function (req:js.node.http.IncomingMessage, res:js.node.http.ServerResponse)
-        handler(
-          new IncomingRequest(
-            req.socket.remoteAddress, 
-            new IncomingRequestHeader(cast req.method, req.url, req.httpVersion, [for (i in 0...Std.int(req.rawHeaders.length / 2)) new HeaderField(req.rawHeaders[2 * i], req.rawHeaders[2 * i +1])]), 
-            Source.ofNodeStream('Incoming HTTP message from ${req.socket.remoteAddress}', req))
-        ).handle(function (out) {
-          res.writeHead(out.header.statusCode, out.header.reason, cast [for (h in out.header.fields) [(h.name : String), h.value]]);//TODO: readable status code
-          out.body.pipeTo(Sink.ofNodeStream('Outgoing HTTP response to ${req.socket.remoteAddress}', res)).handle(function (x) {
-            res.end();
-          });
-        });
-  
-  
-  public function run(application:Application) {
-    var server = js.node.Http.createServer(toNodeHandler(application.serve));
-    application.done.handle(function () { 
-      server.close(); 
-    });
-    server.listen(port);
-    server.on('error', function (e) application.onError(Error.reporter('Failed to bind port $port')(e)));
-  }
+enum ContainerResult {
+  Running(running:RunningState);
+  Failed(e:Error);
+  Done;
 }
-#end
 
-#if (php || neko)
-private typedef Cgi = 
-  #if neko
-    neko.Web;
-  #else
-    php.Web;
-  #end
- 
-class CgiContainer implements Container {
-  function new() { }
-  
-  function getRequest() {
-    return new IncomingRequest(
-      Cgi.getClientIP(),
-      new IncomingRequestHeader(
-        cast Cgi.getMethod(),
-        Cgi.getURI(),
-        'HTTP/1.1',
-        [for (h in Cgi.getClientHeaders()) new HeaderField(h.header, h.value)]
-      ),
-      (Cgi.getPostData() : IdealSource)
-    );
-  }
-  
-  
-  public function run(application:Application) {
-      
-    function doRun() 
-      application.serve(getRequest()).handle(function (response) {
-        Cgi.setReturnCode(response.header.statusCode);
-        for (h in response.header.fields)
-          Cgi.setHeader(h.name, h.value);
-        var out = new BytesOutput();
-        response.body.pipeTo(Sink.ofOutput('buf', out)).handle(function (x) {
-          Sys.print(out.getBytes().toString());
-        });          
-      });
-    
-    
-    #if neko
-      #if tink_runloop
-        Cgi.cacheModule(function () { @:privateAccess tink.RunLoop.current.spin(doRun); } );//this is not exactly pretty, but it should do the job for now
-      #else
-        Cgi.cacheModule(doRun);
-      #end
-    #end
-    doRun();
-  }
-  static public var instance(default, null):Container = new CgiContainer(); 
+typedef RunningState = {
+  var failures(default, null):Signal<ContainerFailure>;
+  function shutdown(hard:Bool):Future<Noise>;
 }
-#end
-class TcpContainer implements Container {
-  
-  var port:Int;
-  var maxConcurrent:Int;
-  @:require(tink_tcp)
-  public function new(port:Int, ?maxConcurrent:Int = 1 << 16) {
-    this.port = port;
-    this.maxConcurrent = maxConcurrent;
-  }
-  
-  public function run(application:Application) {
-    #if tink_tcp
-    tink.tcp.Server.bind(port).handle(function (o) switch o {
-      case Success(server):
-        
-        application.done.handle(server.close);
-        var pending = new List();
-        var current = 0;
-        
-        function serve(cnx:tink.tcp.Connection, next:Void->Void)
-          cnx.source.parse(IncomingRequestHeader.parser()).handle(function (o) switch o {
-            case Success({ data: header, rest: body }):
-              
-              switch header.byName('content-length') {
-                case Success(v):
-                  body = body.limit(Std.parseInt(v));
-                default:
-              }
-              
-              application.serve(new IncomingRequest(cnx.peer.host, header, body)).handle(function (res) {
-                
-                res.body.prepend(res.header.toString()).pipeTo(cnx.sink, { end: true }).handle(function (r) {
-                  next(); 
-                  switch r {
-                    case SinkFailed(e) | SourceFailed(e): application.onError(e);
-                    case SinkEnded: application.onError(new Error('${cnx.peer} hung up before the whole body was written'));
-                    default:
-                  }
-                });
-                
-              });
-            case Failure(e):  
-              application.onError(e);
-              cnx.close();
-              next();
-          });          
-        
-        server.connected.handle(function (cnx) {
-          //serve(cnx, function () { } );
-          //return;
-          pending.add(cnx);
-          
-          function next() 
-            switch pending.pop() {
-              case null:
-                current--;
-              case cnx:
-                serve(cnx, next);
-            }
-            
-          if (current < maxConcurrent) {
-            current++;
-            next();
-          }
-          
-        });
-      case Failure(e):
-        application.onError(e);
-    });
-    #end
-  }
-}
+
+typedef ContainerFailure = { 
+  var error(default, null):Error;
+  var request(default, null):IncomingRequest;
+  var response(default, null):OutgoingResponse;  
+};
---
+++ src/tink/http/containers/TcpContainer.hx
@@ -0,0 +1,117 @@
+package tink.http.containers;
+
+import tink.http.Container;
+import tink.http.Handler;
+import tink.http.Request;
+import tink.http.Response;
+import tink.tcp.Connection;
+
+using tink.CoreApi;
+
+class TcpContainer implements Container {
+  
+  var port:Int;
+  var maxConcurrent:Int;
+  var onInvalidRequest:Null<Error->Connection->Void>;
+  
+  @:require(tink_tcp)
+  public function new(port:Int, maxConcurrent:Int = 0, ?onInvalidRequest) {
+    this.port = port;
+    this.maxConcurrent = maxConcurrent;
+    this.onInvalidRequest = onInvalidRequest;
+  }
+  
+  public function run(handler:Handler):Future<ContainerResult> {
+    return Future.async(function (cb) {
+      #if tink_tcp
+      tink.tcp.Server.bind(port).handle(function (o) switch o {
+        case Success(server):
+          
+          var failures = Signal.trigger();
+          
+          cb(Running({
+            shutdown: function (hard:Bool) {
+              if (!hard)
+                trace('Warning: soft shutdown not implemented');//TODO: implement soft shutdown
+              server.close();
+              return Future.sync(Noise);
+            },
+            failures: failures,
+          }));
+          
+          var pending = new List();
+          var current = 0;
+          
+          function serve(cnx:Connection, ?next:Void->Void)
+            cnx.source.parse(IncomingRequestHeader.parser()).handle(function (o) switch o {
+              case Success({ data: header, rest: body }):
+                
+                switch header.byName('content-length') {
+                  case Success(v):
+                    body = body.limit(Std.parseInt(v));
+                  default:
+                }
+                
+                var req = new IncomingRequest(cnx.peer.host, header, body);
+                
+                handler.process(req).handle(function (res) {
+                  
+                  function fail(e)
+                    failures.trigger({
+                      error: e,
+                      request: req,
+                      response: res,
+                    });
+                  
+                  res.body.prepend(res.header.toString()).pipeTo(cnx.sink, { end: true }).handle(function (r) {
+                    if (next != null)
+                      next(); 
+                    switch r {
+                      case SinkFailed(e) | SourceFailed(e): fail(e);
+                      case SinkEnded: fail(new Error('${cnx.peer} hung up before the whole body was written'));
+                      default:
+                    }
+                  });
+                  
+                });
+              case Failure(e):  
+                
+                switch onInvalidRequest {
+                  case null:
+                  case v: v(e, cnx);
+                }
+                
+                cnx.close();
+                if (next != null)
+                  next();
+            });          
+          
+          server.connected.handle(function (cnx) {
+            if (maxConcurrent <= 0) {
+              serve(cnx);
+              return;
+            }
+            pending.add(cnx);
+            
+            function next() 
+              switch pending.pop() {
+                case null:
+                  current--;
+                case cnx:
+                  serve(cnx, next);
+              }
+              
+            if (current < maxConcurrent) {
+              current++;
+              next();
+            }
+            
+          });
+        case Failure(e):
+          cb(Failed(e));
+          //application.onError(e);
+      });
+      #end
+    });
+  }
+}
