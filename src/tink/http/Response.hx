c5392eaee02c1b0a779e2b5e5a6e56a199c4b0cf
Juraj Kirchheim <back2dos@googlemail.com>
2016-09-21 06:32:43 +0200

    Slightly expand response API.

--- src/tink/http/Response.hx
+++ src/tink/http/Response.hx
@@ -7,6 +7,8 @@ import tink.http.Message;
 import tink.http.Header;
 import tink.io.*;
 
+using tink.CoreApi;
+
 class ResponseHeader extends Header {
   
   public var statusCode(default, null):Int;
@@ -50,9 +52,9 @@ abstract OutgoingResponse(OutgoingResponseData) {
   public inline function new(header, body) 
     this = new OutgoingResponseData(header, body);
     
-  static public function blob(bytes:Bytes, contentType:String)
+  static public function blob(?code = 200, bytes:Bytes, contentType:String)
     return new OutgoingResponse(
-      new ResponseHeader(200, 'OK', [new HeaderField('Content-Type', contentType), new HeaderField('Content-Length', Std.string(bytes.length))]), 
+      new ResponseHeader(code, 'OK', [new HeaderField('Content-Type', contentType), new HeaderField('Content-Length', Std.string(bytes.length))]), 
       bytes
     );
     
@@ -65,7 +67,7 @@ abstract OutgoingResponse(OutgoingResponseData) {
   static public function reportError(e:Error) {
     return new OutgoingResponse(
       new ResponseHeader(e.code, e.message, [new HeaderField('Content-Type', 'application/json')]),
-      haxe.Json.stringify( {
+      haxe.Json.stringify({//TODO: reconsider the wisdom of json encoding this way, since it relies on reflection
         error: e.message,
         details: e.data,
         //TODO: add stack trace when it becomes available
@@ -74,4 +76,15 @@ abstract OutgoingResponse(OutgoingResponseData) {
   }
 }
 
-typedef IncomingResponse = Message<ResponseHeader, Source>;
+class IncomingResponse extends Message<ResponseHeader, Source> {
+  
+  static public function readAll(res:IncomingResponse) 
+    return res.body.all() 
+      >> function (b:Bytes) 
+        return 
+          if (res.header.statusCode >= 400) 
+            Failure(Error.withData(res.header.statusCode, res.header.reason, b.toString()))
+          else
+            Success(b);
+        
+}
