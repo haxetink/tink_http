d2d7dcfc6d48036bc047ff6f32d2bf2051345c54
kevinresol <kevinresol@gmail.com>
2016-10-04 09:27:31 +0800

    Cleanup

--- src/tink/http/Client.hx
+++ src/tink/http/Client.hx
@@ -142,7 +142,7 @@ class PhpClient implements ClientObject {
     return Future.async(function(cb) {
       req.body.all().handle(function(bytes) {
         var options = php.Lib.associativeArrayOfObject({
-        http: php.Lib.associativeArrayOfObject({
+          http: php.Lib.associativeArrayOfObject({
             // protocol_version: // TODO: req does not define the version?
             header: req.header.fields.map(function(f) return f.toString()).join('\r\n') + '\r\n',
             method: req.header.method,
@@ -151,7 +151,7 @@ class PhpClient implements ClientObject {
         });
         var context = untyped __call__('stream_context_create', options);
         var url = '$protocol:' + req.header.fullUri();
-        var result:haxe.io.Input = @:privateAccess new sys.io.FileInput(untyped __call__('fopen', url, 'rb', false, context));
+        var result = @:privateAccess new sys.io.FileInput(untyped __call__('fopen', url, 'rb', false, context));
         var headers:Source = php.Lib.toHaxeArray(untyped __php__("$http_response_header")).join('\r\n') + '\r\n';
         headers.parse(ResponseHeader.parser()).handle(function(o) switch o {
           case Success(parsed):
