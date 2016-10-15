e7976d746c6b74dc891cf783489a705e1bf16fe5
Ben Merckx <ben@codeurs.be>
2016-10-15 23:31:28 +0200

    Tests indentation

--- tests/ProcessTools.hx
+++ tests/ProcessTools.hx
@@ -1,5 +1,3 @@
-package;
-
 import haxe.io.Input;
 import sys.io.File;
 import sys.io.Process;
@@ -10,28 +8,28 @@ import haxe.io.Eof;
 
 class ProcessTools {
 
-	public static function streamAll(cmd, args): Process {
-		var process = new Process(cmd, args);
-		stream(process.stderr);
-		stream(process.stdout);
-		return process;
-	}
+  public static function streamAll(cmd, args): Process {
+    var process = new Process(cmd, args);
+    stream(process.stderr);
+    stream(process.stdout);
+    return process;
+  }
   
   public static function travix(target: String, args: Array<String>): Process {
-    File.saveContent('tests.hxml', ['-cp tests'].concat(args).join('\n'));
-		return streamAll('haxelib', ['run', 'travix', target]);
-	}
-	
-	static function stream(input: Input) {
-		var stdout = Sys.stdout();
-		Thread.create(function()
-			while (true)
-        try {
-          stdout.writeByte(input.readByte());
-          stdout.flush();
-        } catch (e: Eof) 
-          break
-		);
-	}
-	
+  File.saveContent('tests.hxml', ['-cp tests'].concat(args).join('\n'));
+    return streamAll('haxelib', ['run', 'travix', target]);
+  }
+  
+  static function stream(input: Input) {
+    var stdout = Sys.stdout();
+    Thread.create(function()
+      while (true)
+    try {
+      stdout.writeByte(input.readByte());
+      stdout.flush();
+    } catch (e: Eof) 
+      break
+    );
+  }
+  
 }
--- tests/Runner.hx
+++ tests/Runner.hx
@@ -13,73 +13,73 @@ using tink.CoreApi;
 using Lambda;
 
 typedef Request = {
-	?method: Method,
-	uri: String,
-	?headers: Array<HeaderField>,
-	?body: String
+  ?method: Method,
+  uri: String,
+  ?headers: Array<HeaderField>,
+  ?body: String
 }
 
 @colorize
 class Runner extends buddy.SingleSuite {
-	
-	var clients = Context.clients.array();
-		
+  
+  var clients = Context.clients.array();
+    
   public function new() {
-    describe('tink_http', {
+  describe('tink_http', {
+  
+    it('should respond', function (done) 
+    request({uri: '/'}, function (res)
+      return toData(res).map(function(data: Data) {
+      data.uri.should.be('/');
+      return Noise;
+      })
+    ).handle(done)
+    );
     
-      it('should respond', function (done) 
-        request({uri: '/'}, function (res)
-          return toData(res).map(function(data: Data) {
-            data.uri.should.be('/');
-            return Noise;
-          })
-        ).handle(done)
-      );
-      
-      it('should return the http method', function (done) 
-        request({uri: '/', method: GET}, function (res)
-          return toData(res).map(function(data: Data) {
-            data.method.should.be('GET');
-            return Noise;
-          })
-        ).handle(done)
-      );
-    
+    it('should return the http method', function (done) 
+    request({uri: '/', method: GET}, function (res)
+      return toData(res).map(function(data: Data) {
+      data.method.should.be('GET');
+      return Noise;
+      })
+    ).handle(done)
+    );
+  
+  });
+  }
+  
+  function toData(res: IncomingResponse): Future<Data>
+    return res.body.all().map(function (o) {
+      var raw: String = o.sure().toString();
+      var data: Data = null;
+    try
+    data = Json.parse(raw)
+    catch (e: Dynamic)
+    throw 'Could not parse response as json:\n$raw\n\n$e';
+      return data;
     });
-	}
-	
-	function toData(res: IncomingResponse): Future<Data>
-		return res.body.all().map(function (o) {
-			var raw: String = o.sure().toString();
-			var data: Data = null;
-      try
-        data = Json.parse(raw)
-      catch (e: Dynamic)
-        throw 'Could not parse response as json:\n$raw\n\n$e';
-			return data;
-		});
-	
-	function request(req: Request, test: IncomingResponse -> Future<Noise>)
-		return Future.ofMany(clients.map(function(client) {
-			var fields = switch req.headers {
-				case null: [];
-				case v: v.copy();
-			}
-			if (req.body == null) 
-				req.body = '';
-			if (req.method == null) 
-				req.method = GET;
-			var outgoing = new OutgoingRequest(new OutgoingRequestHeader(req.method, new Host('127.0.0.1', Std.parseInt(Env.getDefine('port'))), req.uri, fields), req.body);
-			switch req.body.length {
-				case 0:
-				case v:
-					switch outgoing.header.get('content-length') {
-						case []:
-							fields.push(new HeaderField('content-length', Std.string(v)));
-						default:
-					}
-			}
-			return client.request(outgoing).flatMap(test);
-		}));
-		
+  
+  function request(req: Request, test: IncomingResponse -> Future<Noise>)
+    return Future.ofMany(clients.map(function(client) {
+      var fields = switch req.headers {
+        case null: [];
+        case v: v.copy();
+      }
+      if (req.body == null) 
+        req.body = '';
+      if (req.method == null) 
+        req.method = GET;
+      var outgoing = new OutgoingRequest(new OutgoingRequestHeader(req.method, new Host('127.0.0.1', Std.parseInt(Env.getDefine('port'))), req.uri, fields), req.body);
+      switch req.body.length {
+        case 0:
+        case v:
+          switch outgoing.header.get('content-length') {
+            case []:
+              fields.push(new HeaderField('content-length', Std.string(v)));
+            default:
+          }
+      }
+      return client.request(outgoing).flatMap(test);
+    }));
+    
 }
