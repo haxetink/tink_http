bb0620331bc936e64d26e68ec8f13c937139b359
Kevin Leung <kevinresol@gmail.com>
2016-12-13 22:26:02 +0800

    Update doc

--- src/tink/http/containers/JavaServletContainer.hx
+++ src/tink/http/containers/JavaServletContainer.hx
@@ -12,45 +12,55 @@ import tink.io.java.*;
 using tink.CoreApi;
 
 /**
-	How to use:
+	Example usage (with Tomcat):
 	
 	1. Write some haxe code as follow:
-	
-	```haxe
-	package;
-
-	import java.javax.servlet.http.*;
-	import tink.http.containers.JavaServletContainer;
-	import tink.http.Request;
-	import tink.http.Response;
-	using tink.CoreApi;
-
-	class Main extends HttpServlet {	
-		@:overload
-		override function service(req:HttpServletRequest, res:HttpServletResponse) {
-			return JavaServletContainer.toServletHandler(handle)(req, res);
-		}
 		
-		function handle(req:IncomingRequest):Future<OutgoingResponse>
-			return Future.sync(('Done':OutgoingResponse));
-	}
-	```
-	
-	2. Add the following xml to web.xml, put them inside the <web-app> tag, leaving other entries intact.
-	
-	```xml
-	<servlet>
-		<servlet-name>Main</servlet-name>
-		<servlet-class>haxe.root.Main</servlet-class>
-	</servlet>
+		```haxe
+		package;
 
-	<servlet-mapping>
-		<servlet-name>Main</servlet-name>
-		<url-pattern>/</url-pattern>
-	</servlet-mapping>
-	```
+		import java.javax.servlet.http.*;
+		import tink.http.containers.JavaServletContainer;
+		import tink.http.Request;
+		import tink.http.Response;
+		using tink.CoreApi;
+
+		class Main extends HttpServlet {	
+			@:overload
+			override function service(req:HttpServletRequest, res:HttpServletResponse) {
+				JavaServletContainer.toServletHandler(handle)(req, res);
+			}
+			
+			function handle(req:IncomingRequest):Future<OutgoingResponse>
+				return Future.sync(('Done':OutgoingResponse));
+		}
+		```
 	
-	3. Start you Servlet container (e.g. Tomcat) and use a browser navigate to the default address (e.g. localhost:8080)
+	2. Compile with `haxe Main -java bin/java`.
+	   Don't put `-main` otherwise the compiler will complain for a missing entry point,
+	   Move the everything under `bin/java/obj` to where your Servlet Container serves.
+	   For Tomcat it will be `<tomcat installation path>/webapps/ROOT/WEB-INF/classes`
+	   (So you should have `<tomcat installation path>/webapps/ROOT/WEB-INF/classes/haxe/root/Main.class`)
+	
+	2. Add the following xml to web.xml.
+	   Put them inside the <web-app> tag, leaving other entries intact.
+	   For Tomcat `web.xml` should be located at `<tomcat installation path>/webapps/ROOT/WEB-INF/`
+	
+		```xml
+		<servlet>
+			<servlet-name>Main</servlet-name>
+			<servlet-class>haxe.root.Main</servlet-class>
+		</servlet>
+
+		<servlet-mapping>
+			<servlet-name>Main</servlet-name>
+			<url-pattern>/</url-pattern>
+		</servlet-mapping>
+		```
+	
+	3. Start you Servlet container and use a browser navigate to the default address
+	   For Tomcat, run `<tomcat installation path>/bin/startup.sh` (or `startup.bat`)
+	   and then visit `localhost:8080`
 	
 **/
 
