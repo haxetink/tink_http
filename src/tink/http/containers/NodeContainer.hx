21be66f077ea402ee8b622af1161682edbe323a8
Kevin Leung <kevinresol@gmail.com>
2017-01-12 21:22:54 +0800

    Enhance node container

--- src/tink/http/containers/NodeContainer.hx
+++ src/tink/http/containers/NodeContainer.hx
@@ -4,20 +4,21 @@ import tink.http.Container;
 import tink.http.Request;
 import tink.http.Header;
 import tink.io.*;
+import js.node.http.*;
 
 using tink.CoreApi;
 
 class NodeContainer implements Container {
   
-  var port:Int;
+  var kind:ServerKind;
   
-  public function new(port) {
-    this.port = port;
+  public function new(kind:ServerKind) {
+    this.kind = kind;
   }
   
   static public function toNodeHandler(handler:Handler)
     return 
-      function (req:js.node.http.IncomingMessage, res:js.node.http.ServerResponse)
+      function (req:IncomingMessage, res:ServerResponse)
         handler.process(
           new IncomingRequest(
             req.socket.remoteAddress, 
@@ -34,16 +35,36 @@ class NodeContainer implements Container {
   public function run(handler:Handler) 
     return Future.async(function (cb) {
       var failures = Signal.trigger();
-      var server = js.node.Http.createServer(toNodeHandler(handler));
+      
+      var server = switch kind {
+        case Instance(server):
+          server;
+          
+        case Port(port):
+          var server = new Server();
+          server.listen(port);
+          server;
+          
+        case Host(host):
+          var server = new Server();
+          server.listen(host.port, host.name);
+          server;
+          
+        case Path(path):
+          var server = new Server();
+          server.listen(path);
+          server;
+          
+        case Fd(fd):
+          var server = new Server();
+          server.listen(fd);
+          server;
+      }
       
       function tinkify(e:js.Error)
         return Error.withData(e.message, e);
         
-      server.on('error', function (e) {
-        cb(Failed(e));
-      });
-      
-      server.listen(port, function () {
+      function onListen() {
         cb(Running({ 
           shutdown: function (hard:Bool) {
             if (hard)
@@ -55,6 +76,44 @@ class NodeContainer implements Container {
           },
           failures: failures,//TODO: these need to be triggered
         }));
-      });      
+      }
+      
+      if(untyped server.listening) // .listening added in v5.7.0, not added to hxnodejs yet
+        onListen()
+      else
+        server.on('listening', onListen);
+      
+      server.on('request', toNodeHandler(handler));
+      server.on('error', function(e) cb(Failed(e)));
     });
+}
+
+private enum ServerKindBase {
+  Instance(server:Server);
+  Port(port:Int);
+  Host(host:tink.url.Host);
+  Path(path:String);
+  Fd(fd:{fd:Int});
+}
+
+abstract ServerKind(ServerKindBase) from ServerKindBase to ServerKindBase {
+  @:from
+  public static function fromInstance(server:Server):ServerKind
+    return Instance(server);
+    
+  @:from
+  public static inline function fromPort(port:Int):ServerKind
+    return Port(port);
+  
+  @:from
+  public static function fromHost(host:tink.url.Host):ServerKind
+    return Host(host);
+  
+  @:from
+  public static function fromPath(path:String):ServerKind
+    return Path(path);
+  
+  @:from
+  public static function fromFd(fd:{fd:Int}):ServerKind
+    return Fd(fd);
 }
