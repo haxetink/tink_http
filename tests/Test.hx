bceb13cb2f94a0b8805b6b0e21914122b3afdc03
back2dos <back2dos@gmail.com>
2017-02-15 18:49:03 +0100

    Begin to make TCP container work again.

--- tests/Test.hx
+++ tests/Test.hx
@@ -1,8 +1,16 @@
 package ;
 
 import tink.http.containers.NodeContainer;
+import tink.http.containers.TcpContainer;
+import tink.http.Response;
+
+using tink.CoreApi;
 
 class Test {
   static function main() {
+    var c = new NodeContainer(12345);
+    c.run(function (req) {
+      return Future.sync(('hello, world' : OutgoingResponse));
+    });
   }
 }
