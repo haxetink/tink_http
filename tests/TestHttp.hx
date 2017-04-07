006ef68899cdef72d53941fb3a87f53b5c4fc53f
Kevin Leung <kevinresol@gmail.com>
2017-04-07 21:25:27 +0800

    Test more methods

--- tests/TestHttp.hx
+++ tests/TestHttp.hx
@@ -1,12 +1,15 @@
 package;
 
 import haxe.DynamicAccess;
+import tink.http.Method;
 import tink.http.Client;
 import tink.http.Response;
 import tink.http.Request;
 import tink.http.Header;
+import tink.Chunk;
 import tink.Url;
 import tink.url.*;
+import tink.unit.*;
 
 using tink.io.Source;
 using tink.CoreApi;
@@ -26,34 +29,58 @@ class TestHttp {
         converter = Converters.httpbin;
     }
   }
+    
+  public function get() return testMethod(GET);
+  public function post() return testMethod(POST);
+  public function patch() return testMethod(PATCH);
+  public function delete() return testMethod(DELETE);
+  public function put() return testMethod(PUT);
   
-  public function get() {
-    client.request(new OutgoingRequest(
-      new OutgoingRequestHeader(GET, url + '/get?a=1&b=2', []),
-      Source.EMPTY
-    )).flatMap(converter).handle(function(o) switch o {
-      case Success(echo):
-        asserts.assert(echo.query.get('a') == '1');
-        asserts.assert(echo.query.get('b') == '2');
-        asserts.done();
-      case Failure(e):
-        asserts.fail(e);
-    });
+  public function header() {
+    request(GET, url + '/headers', [new HeaderField('x-custom-tink', 'tink_http')])
+      .handle(function(o) switch o {
+        case Success(echo):
+          asserts.assert(Type.enumEq(echo.headers.byName('x-custom-tink'), Success('tink_http')));
+          asserts.done();
+        case Failure(e):
+          asserts.fail(e);
+      });
     return asserts;
   }
   
-  public function header() {
-    client.request(new OutgoingRequest(
-      new OutgoingRequestHeader(GET, url + '/headers', [new HeaderField('x-custom-tink', 'tink_http')]),
-      Source.EMPTY
-    )).flatMap(converter).handle(function(o) switch o {
-      case Success(echo):
-        asserts.assert(Type.enumEq(echo.headers.byName('x-custom-tink'), Success('tink_http')));
-        asserts.done();
-      case Failure(e):
-        asserts.fail(e);
-    });
-    return asserts;
+  
+  
+  function request(method:Method, url:Url, ?headers:Array<HeaderField>, ?body:IdealSource) {
+    // trace(url);
+    return client.request(new OutgoingRequest(
+      new OutgoingRequestHeader(method, url, headers),
+      body == null ? Source.EMPTY : body
+    )).flatMap(converter);
+  }
+  
+  function testMethod(method:Method) {
+    var asserts = new AssertionBuffer();
+    var body:String = null;
+    var headers = null;
+    switch method {
+      case GET: // do nothing
+      default: 
+        body = 'tink_http $method';
+        headers = [
+          new HeaderField('content-type', 'text/plain'),
+          new HeaderField('content-length', Std.string(body.length)),
+        ];
+    }
+    return request(method, url + '/${(method:String).toLowerCase()}?a=1&b=2', headers, body == null ? null : body)
+      .map(function(o) return switch o {
+        case Success(echo):
+          asserts.assert(echo.query.get('a') == '1');
+          asserts.assert(echo.query.get('b') == '2');
+          if(body != null) asserts.assert(echo.body == body);
+          Success(asserts.done());
+        case Failure(e):
+          Success(asserts.fail(e));
+      });
   }
 }
 
@@ -63,10 +90,12 @@ enum Target {
 
 class Converters {
   public static function httpbin(res:IncomingResponse):Promise<EchoedRequest> {
-    return res.body.all().next(function(chunk) {
+    return res.body.all().next(function(chunk):EchoedRequest {
+      // trace(chunk);
       var parsed: {
         headers:DynamicAccess<String>,
         args:DynamicAccess<String>,
+        data:String,
       } = haxe.Json.parse(chunk);
       return {
         headers: new Header(
@@ -79,7 +108,8 @@ class Converters {
           var map = new Map();
           if(parsed.args != null) for(name in parsed.args.keys()) map.set(name, parsed.args.get(name));
           map;
-        }
+        },
+        body: parsed.data == null ? Chunk.EMPTY : parsed.data,
       }
     });
   }
@@ -88,4 +118,5 @@ class Converters {
 typedef EchoedRequest = {
   headers:Header,
   query:Map<String, String>,
+  body:Chunk,
 }
