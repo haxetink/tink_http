c38aaf840d5b30c71966886200334602fcf78c46
Kevin Leung <kevinresol@gmail.com>
2017-05-08 23:35:47 +0800

    Works

--- src/tink/http/clients/FlashClient.hx
+++ src/tink/http/clients/FlashClient.hx
@@ -22,44 +22,55 @@ class FlashClient implements ClientObject {
   
   public function request(req:OutgoingRequest):Promise<IncomingResponse> {
     return Future.async(function(cb) {
+      
+      switch req.header.byName('connection') {
+        case Success((_:String).toLowerCase() => 'close'): // ok
+        case Success(v):
+          cb(Failure(new Error('Only "Connection: Close" is supported. But specified as "$v"')));
+          return;
+        case Failure(_): @:privateAccess req.header.fields.push(new HeaderField('connection', 'close'));
+      }
+      
       var socket = new Socket();
       
       var accumulator = new Accumulator<Chunk, Error>();
       socket.addEventListener(ProgressEvent.SOCKET_DATA, function(e:ProgressEvent) {
-        trace('ProgressEvent.SOCKET_DATA', e);
         var len:Int = socket.bytesAvailable;
         var buf = Bytes.alloc(len);
         socket.readBytes(buf.getData(), 0, len);
         accumulator.yield(Data((buf:Chunk)));
       });
       socket.addEventListener(IOErrorEvent.IO_ERROR, function(e:IOErrorEvent) {
-        trace('IOErrorEvent.IO_ERROR', e);
         accumulator.yield(Fail(Error.withData('Error reading from ${req.header.url}', e)));
       });
       socket.addEventListener(SecurityErrorEvent.SECURITY_ERROR, function(e:SecurityErrorEvent) {
-        trace('SecurityErrorEvent.SECURITY_ERROR', e);
         accumulator.yield(Fail(Error.withData('Error reading from ${req.header.url}', e)));
       });
       socket.addEventListener(Event.CLOSE, function(e:Event) {
-        trace('Event.CLOSE', e);
         accumulator.yield(End);
       });
       socket.addEventListener(Event.CONNECT, function(e:Event) {
-        trace('Event.CONNECT', e);
         req.body.prepend(req.header.toString()).chunked().forEach(function(chunk:Chunk) {
           socket.writeBytes(chunk.toBytes().getData(), 0, chunk.length);
-          trace('write ${chunk.length} bytes');
-          socket.flush();
           return Resume;
         }).handle(function(o) switch o {
-          case Depleted: // ok
+          case Depleted: socket.flush();
           case Halted(_): throw 'unreachable';
         });
       });
       
       var source:RealSource = accumulator;
       source.parse(ResponseHeader.parser()).handle(function(o) switch o {
-        case Success(parsed): cb(Success(new IncomingResponse(parsed.a, parsed.b)));
+        case Success(parsed):
+          var len = switch parsed.a.byName(CONTENT_LENGTH) {
+            case Success(v):
+              switch Std.parseInt(v) {
+                case null: cb(Failure(new Error('Invalid Content-Length Header "$v"'))); return;
+                case len: len;
+              }
+            case Failure(_): 0; // assume 0 is ok?
+          }
+          cb(Success(new IncomingResponse(parsed.a, parsed.b.limit(len))));
         case Failure(e): cb(Failure(e));
       });
       
@@ -67,6 +78,10 @@ class FlashClient implements ClientObject {
         case null: secure ? 443 : 80;
         case v: v;
       }
+      
+      // flash.system.Security.allowDomain('*');
+      // flash.system.Security.allowInsecureDomain('*');
+      // flash.system.Security.loadPolicyFile('http://localhost:8000/crossdomain.xml');
       
       try {
         socket.connect(req.header.url.host.name, port);
