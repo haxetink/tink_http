284e75359e942c77ab5f08fa4237d0e75845fe70
Kevin Leung <kevinresol@gmail.com>
2017-09-27 21:36:11 +0800

    Make reason optional too

--- src/tink/http/Response.hx
+++ src/tink/http/Response.hx
@@ -17,9 +17,9 @@ class ResponseHeader extends Header {
   public var reason(default, null):Reason;
   public var protocol(default, null):String;
   
-  public function new(statusCode:StatusCode, reason:Reason, ?fields, ?protocol:Protocol = HTTP1_1) {
+  public function new(statusCode:StatusCode, ?reason:Reason, ?fields, ?protocol:Protocol = HTTP1_1) {
     this.statusCode = statusCode;
-    this.reason = reason;
+    this.reason = reason == null ? statusCode : reason;
     this.protocol = protocol;
     super(fields);
   }
