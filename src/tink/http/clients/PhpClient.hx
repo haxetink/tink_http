f2c5a2be2ba66296feb9c1dae6be255393fb6e11
Kevin Leung <kevinresol@gmail.com>
2018-07-20 01:40:08 +0800

    Update php client (but not working still)

--- src/tink/http/clients/PhpClient.hx
+++ src/tink/http/clients/PhpClient.hx
@@ -1,36 +1,36 @@
 package tink.http.clients;
 
+import tink.http.Client;
+import tink.http.Response;
+import tink.http.Request;
+
+using tink.io.Source;
+using tink.CoreApi;
 
 class PhpClient implements ClientObject {
   var protocol:String = 'http';
   public function new() {}
   
-  public function request(req:OutgoingRequest):Future<IncomingResponse> {
+  public function request(req:OutgoingRequest):Promise<IncomingResponse> {
     return Future.async(function(cb) {
-      req.body.all().handle(function(bytes) {
+      req.body.all().handle(function(chunk) {
         var options = php.Lib.associativeArrayOfObject({
           http: php.Lib.associativeArrayOfObject({
             // protocol_version: // TODO: req does not define the version?
-            header: req.header.fields.map(function(f) return f.toString()).join('\r\n') + '\r\n',
+            header: [for(h in req.header) h.toString()].join('\r\n') + '\r\n',
             method: req.header.method,
-            content: cast bytes.getData()
+            content: chunk.toBytes().getData()
           }),
         });
         var context = untyped __call__('stream_context_create', options);
-        var url = '$protocol:' + req.header.fullUri();
+        var url = '$protocol:' + req.header.url;
         var result = @:privateAccess new sys.io.FileInput(untyped __call__('fopen', url, 'rb', false, context));
-        var headers:Source = php.Lib.toHaxeArray(untyped __php__("$http_response_header")).join('\r\n') + '\r\n';
+        var headers:IdealSource = php.Lib.toHaxeArray(untyped __php__("$http_response_header")).join('\r\n') + '\r\n';
         headers.parse(ResponseHeader.parser()).handle(function(o) switch o {
           case Success(parsed):
-            cb(new IncomingResponse(
-              parsed.data,
-              result.readAll()
-            ));
+            cb(Success(new IncomingResponse(parsed.a, result.readAll())));
           case Failure(e):
-            cb(new IncomingResponse(
-              new ResponseHeader(500, 'Header parse error', []),
-              Std.string(e)
-            ));
+            cb(Failure(e));
         });
       });
     });
