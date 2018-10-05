e90f4b0bf34977919cac4ad7c8fe1cc61c55ed7d
Kevin Leung <kevinresol@gmail.com>
2018-10-05 12:29:59 +0800

    Fix js

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -22,7 +22,10 @@ class RunTests {
     
     #if !no_client
     for(client in Context.clients) {
+      #if ((nodejs || sys) && !php)
       if(client == Curl) continue; // CurlClient can't parse header due to https://github.com/haxetink/tink_http/issues/97
+      #end
+      
       #if !container_only
       tests = tests.concat([
         TestSuite.make(new TestHttp(client, Httpbin, false), '$client -> http://httpbin.org'),
