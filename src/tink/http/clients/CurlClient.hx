efa32a3adf55bc762c607575f4c43eb1c555f2a6
Kevin Leung <kevinresol@gmail.com>
2018-10-12 11:20:26 +0800

    Fix lua https://github.com/HaxeFoundation/haxe/issues/7539

--- src/tink/http/clients/CurlClient.hx
+++ src/tink/http/clients/CurlClient.hx
@@ -11,26 +11,13 @@ using tink.CoreApi;
 
 // Does not restrict to any platform as long as they can run the curl command somehow
 class CurlClient implements ClientObject {
-  var curl:Array<String>->IdealSource->RealSource;
+  
   var protocol:String = 'http';
+  
   public function new(?curl:Array<String>->IdealSource->RealSource) {
-    this.curl = 
-      if(curl != null) curl;
-      else {
-        #if (sys || nodejs)
-          function(args, body) {
-            args.push('--data-binary');
-            args.push('@-');
-            var process = #if sys new sys.io.Process #elseif nodejs js.node.ChildProcess.spawn #end ('curl', args);
-            var sink = #if sys Sink.ofOutput #else Sink.ofNodeStream #end ('stdin', process.stdin);
-            body.pipeTo(sink, {end: true}).eager();
-            return #if sys Source.ofInput #else Source.ofNodeStream #end ('stdout', process.stdout);
-          }
-        #else
-          throw "curl function not supplied";
-        #end
-      }
+    if(curl != null) this.curl = curl;
   }
+  
   public function request(req:OutgoingRequest):Promise<IncomingResponse> {
     var args = [];
     
@@ -56,5 +43,21 @@ class CurlClient implements ClientObject {
     return curl(args, req.body)
       .parse(ResponseHeader.parser())
       .next(function (p) return new IncomingResponse(p.a, p.b));
+  }
+  
+  dynamic function curl(args:Array<String>, body:IdealSource):RealSource {
+    #if (sys || nodejs)
+      args.push('--data-binary');
+      args.push('@-');
+      var process = #if sys new sys.io.Process #elseif nodejs js.node.ChildProcess.spawn #end ('curl', args);
+      var sink = #if sys Sink.ofOutput #else Sink.ofNodeStream #end ('stdin', process.stdin);
+      body.pipeTo(sink, {end: true}).handle(function(o) trace(o));
+      
+      var ret = #if sys Source.ofInput #else Source.ofNodeStream #end ('stdout', process.stdout);
+      ret.all().handle(function(o) trace(o.sure()));
+      return ret;
+    #else
+      throw "curl function not supplied";
+    #end
   }
 }
