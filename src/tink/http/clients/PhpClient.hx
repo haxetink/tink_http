aaae65265e3fd56741cf516101a2b4d868d1de57
postite <blog@postite.com>
2019-03-06 15:00:20 +0100

    remove phpwarnings for haxe4+ (#108)
    * remove phpwarnings for haxe4+
    
    * typo
    
    * imports for haxe4 only
    
    * forgot one
    
    * relaunch

--- src/tink/http/clients/PhpClient.hx
+++ src/tink/http/clients/PhpClient.hx
@@ -1,57 +1,65 @@
-package tink.http.clients;
-
-import tink.http.Client;
-import tink.http.Response;
-import tink.http.Request;
-import tink.http.Header;
-
-using tink.io.Source;
-using tink.CoreApi;
-using StringTools;
-
-class PhpClient implements ClientObject {
-  public function new() {}
-  
-  public function request(req:OutgoingRequest):Promise<IncomingResponse> {
-    return Future.async(function(cb) {
-      req.body.all().handle(function(chunk) {
-        var options = php.Lib.associativeArrayOfObject({
-          http: php.Lib.associativeArrayOfObject({
-            // protocol_version: // TODO: req does not define the version?
-            header: [for(h in req.header) h.toString()].join('\r\n') + '\r\n',
-            method: req.header.method,
-            content: chunk.toBytes().getData().toString(),
-          }),
+    package tink.http.clients;
+    
+    import tink.http.Client;
+    import tink.http.Response;
+    import tink.http.Request;
+    import tink.http.Header;
+    
+    using tink.io.Source;
+    using tink.CoreApi;
+    using StringTools;
+    
+    class PhpClient implements ClientObject {
+      public function new() {}
+      
+      public function request(req:OutgoingRequest):Promise<IncomingResponse> {
+        return Future.async(function(cb) {
+          req.body.all().handle(function(chunk) {
+            var options = php.Lib.associativeArrayOfObject({
+              http: php.Lib.associativeArrayOfObject({
+                // protocol_version: // TODO: req does not define the version?
+                header: [for(h in req.header) h.toString()].join('\r\n') + '\r\n',
+                method: req.header.method,
+                content: chunk.toBytes().getData().toString(),
+              }),
+            });
+            
+            var context = #if haxe4 php.Syntax.code('stream_context_create({0})', options) #else untyped __call__('stream_context_create', options) #end;
+            var url:String = req.header.url;
+            //var result = @:privateAccess new sys.io.FileInput(untyped __call__('fopen', url, 'rb', false, context));
+            var result = @:privateAccess new sys.io.FileInput(
+              #if haxe4  php.Global.fopen( #else untyped __call__('fopen', #end url, 'rb', false, context));
+            
+            var rawHeaders:Array<String> = cast php.Lib.toHaxeArray(#if haxe4 php.Syntax.code #else untyped __php__ #end("$http_response_header"));
+            
+            // http://php.net/manual/en/reserved.variables.httpresponseheader.php#122362
+            // $http_response_header includes all the "history" headers in case of redirected response
+            var i = rawHeaders.length;
+            while(i-- >= 0) if(rawHeaders[i].startsWith('HTTP/')) break;
+            rawHeaders = rawHeaders.slice(i);
+            
+            // construct the header object
+            var head = rawHeaders[0].split(' ');
+            var headers =  [for(i in 1...rawHeaders.length) {
+              var line = rawHeaders[i];
+              var index = line.indexOf(': ');
+              var name = line.substr(0, index);
+               new HeaderField(
+                name,
+                line.substr(index + 2)
+                );
+            }];
+            
+            var header = new ResponseHeader(Std.parseInt(head[1]), head.slice(2).join(' '),  headers);
+            cb(Success(new IncomingResponse(header, result.readAll())));
+            
+            // var headers:IdealSource = php.Lib.toHaxeArray(untyped __php__("$http_response_header")).join('\r\n') + '\r\n';
+            // headers.parse(ResponseHeader.parser()).handle(function(o) switch o {
+            //   case Success(parsed):
+            //   case Failure(e):
+            //     cb(Failure(e));
+            // });
+          });
         });
-        var context = untyped __call__('stream_context_create', options);
-        var url:String = req.header.url;
-        var result = @:privateAccess new sys.io.FileInput(untyped __call__('fopen', url, 'rb', false, context));
-        
-        var rawHeaders:Array<String> = cast php.Lib.toHaxeArray(untyped __php__("$http_response_header"));
-        
-        // http://php.net/manual/en/reserved.variables.httpresponseheader.php#122362
-        // $http_response_header includes all the "history" headers in case of redirected response
-        var i = rawHeaders.length;
-        while(i-- >= 0) if(rawHeaders[i].startsWith('HTTP/')) break;
-        rawHeaders = rawHeaders.slice(i);
-        
-        // construct the header object
-        var head = rawHeaders[0].split(' ');
-        var headers = [for(i in 1...rawHeaders.length) {
-          var line = rawHeaders[i];
-          var index = line.indexOf(': ');
-          new HeaderField(line.substr(0, index), line.substr(index + 2));
-        }];
-        var header = new ResponseHeader(Std.parseInt(head[1]), head.slice(2).join(' '), headers);
-        cb(Success(new IncomingResponse(header, result.readAll())));
-        
-        // var headers:IdealSource = php.Lib.toHaxeArray(untyped __php__("$http_response_header")).join('\r\n') + '\r\n';
-        // headers.parse(ResponseHeader.parser()).handle(function(o) switch o {
-        //   case Success(parsed):
-        //   case Failure(e):
-        //     cb(Failure(e));
-        // });
-      });
-    });
-  }
-}
+      }
+    }
