f4cfb782cdef6ab7cf7d9ef85663f43b2774ee15
Kevin Leung <kevinresol@gmail.com>
2019-09-28 22:58:17 +0800

    Add FirebaseFunctionContainer

---
+++ src/tink/http/containers/FirebaseFunctionContainer.hx
@@ -0,0 +1,81 @@
+package tink.http.containers;
+
+import tink.http.Container;
+
+using tink.io.Source;
+using tink.CoreApi;
+
+class FirebaseFunctionContainer implements Container {
+	var name:String;
+	var regions:Regions;
+	var options:RuntimeOptions;
+	
+	public function new(name, ?regions, ?options) {
+		this.name = name;
+		this.regions = regions;
+		this.options = options;
+	}
+	
+	public function run(handler:Handler):Future<ContainerResult> {
+		return Future.async(function(cb) {	
+			Reflect.setField(
+				js.Node.exports,
+				name,
+				{
+					var f:FunctionBuilder = FirebaseFunctions;
+					if(regions != null) f = f.region(js.Syntax.code('...{0}', regions));
+					if(options != null) f = f.runWith(options);
+					f.https.onRequest(NodeContainer.toNodeHandler(
+						handler, {
+							// https://firebase.google.com/docs/functions/http-events#read_values_from_the_request
+							body: function(msg) {
+								var buffer:js.node.Buffer = untyped msg.rawBody;
+								return Plain(buffer == null ? Source.EMPTY : (buffer:tink.Chunk));
+							}
+						}
+					));
+				}
+			);
+			
+			// firebase function will kill this node process when the request is done
+			js.Node.process.on('exit', cb.bind(Shutdown));
+		});
+		
+	}
+}
+
+private abstract Regions(Array<String>) from Array<String> to Array<String>{
+	@:from public static inline function fromString(region:String):Regions return [region];
+}
+
+private typedef Common = {
+	var https:HttpsFunction;
+	function region(regions:haxe.extern.Rest<String>):FunctionBuilder;
+	function runWith(options:RuntimeOptions):FunctionBuilder;
+}
+
+// https://github.com/firebase/firebase-functions/blob/master/src/index.ts
+@:jsRequire('firebase-functions')
+private extern class FirebaseFunctions {
+	static var https:HttpsFunction;
+	static function region(regions:haxe.extern.Rest<String>):FunctionBuilder;
+	static function runWith(options:RuntimeOptions):FunctionBuilder;
+}
+
+// https://github.com/firebase/firebase-functions/blob/master/src/function-builder.ts
+private typedef FunctionBuilder = {
+	var https:HttpsFunction;
+	function region(regions:haxe.extern.Rest<String>):FunctionBuilder;
+	function runWith(options:RuntimeOptions):FunctionBuilder;
+}
+
+// https://github.com/firebase/firebase-functions/blob/master/src/providers/https.ts
+private extern class HttpsFunction {
+	function onRequest(handler:js.node.http.IncomingMessage->js.node.http.ServerResponse->Void):HttpsFunction;
+}
+
+// https://github.com/firebase/firebase-functions/blob/master/src/function-configuration.ts
+private typedef RuntimeOptions = {
+	?memory:String,
+	?timeoutSeconds:Int,
+}
