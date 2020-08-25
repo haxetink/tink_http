3f018aab66d75fcb5ad186898b0acb9c172c2e4d
Kevin Leung <kevinresol@gmail.com>
2020-08-25 17:25:21 +0800

    Temporarily disable redirect test on httpbin see https://github.com/postmanlabs/httpbin/issues/617

--- tests/FetchTest.hx
+++ tests/FetchTest.hx
@@ -29,9 +29,11 @@ class FetchTest {
   public function patch() return testData('http://httpbin.org/patch', PATCH);
   public function put() return testData('http://httpbin.org/put', PUT);
   
-  #if !cpp // TODO: investigate
-  public function redirect() return testStatus('http://httpbin.org/redirect/5');
-  #end
+  
+  // TODO: re-enable when issue resolved: https://github.com/postmanlabs/httpbin/issues/617
+  // #if !cpp // TODO: investigate
+  // public function redirect() return testStatus('http://httpbin.org/redirect/5');
+  // #end
   
   #if(!python && !cs && !interp && !lua)
   public function secureGet() return testStatus('https://httpbin.org/');
@@ -39,9 +41,11 @@ class FetchTest {
   public function secureDelete() return testData('https://httpbin.org/delete', DELETE);
   public function securePatch() return testData('https://httpbin.org/patch', PATCH);
   public function securePut() return testData('https://httpbin.org/put', PUT);
-  #if !cpp // TODO: investigate
-  public function secureRedirect() return testStatus('https://httpbin.org/redirect/5');
-  #end
+  
+  // TODO: re-enable when issue resolved: https://github.com/postmanlabs/httpbin/issues/617
+  // #if !cpp // TODO: investigate
+  // public function secureRedirect() return testStatus('https://httpbin.org/redirect/5');
+  // #end
   #end
   
   public function headers(buffer:AssertionBuffer) {
